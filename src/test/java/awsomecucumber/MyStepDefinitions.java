3ff34c456b046f5f7a2660d4140fb201dedcd231
chamaraf <developandtestanddeploy@gmail.com>
2024-02-05 20:47:11 +0000

    Base page and driverfactory added

--- src/test/java/awsomecucumber/MyStepDefinitions.java
+++ src/test/java/awsomecucumber/MyStepDefinitions.java
@@ -1,5 +1,6 @@
 package awsomecucumber;
 
+import awsomecucumber.factory.DriverFactory;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -8,7 +9,10 @@ import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
+import java.util.List;
+import java.util.Map;
 import java.util.TreeMap;
 
 public class MyStepDefinitions {
@@ -18,9 +22,9 @@ public class MyStepDefinitions {
     @Given("I am on the store page")
     public void i_am_on_the_store_page() {
 
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
+        driver = DriverFactory.getDriver();
         driver.get("https://askomdch.com/store");
+        System.out.println("adaaff");
        
     }
     @When("I add {string} to the cart")
@@ -45,6 +49,9 @@ public class MyStepDefinitions {
 //new steps
     @Given("I'm a guest customer")
     public void iMAGuestCustomer() {
+
+        driver = DriverFactory.getDriver();
+        driver.get("https://askomdch.com/store");
     }
 
     @And("I have a product in the cart")
@@ -59,17 +66,49 @@ public class MyStepDefinitions {
 
     @And("I am on the checkout page")
     public void iAmOnTheCheckoutPage() {
+        By proceedToCheckoutBtn = By.cssSelector(".checkout-button");
+        driver.findElement(proceedToCheckoutBtn).click();
     }
 
     @When("I provide billing details")
-    public void iProvideBillingDetails() {
+    public void iProvideBillingDetails(List<Map<String , String>> billingDetails) {
+        By billingFirstNameFld = By.id("billing_first_name");
+        By billingLastNameFld = By.id("billing_last_name");
+        By billingAddressOneFld = By.id("billing_address_1");
+        By billingCityFld = By.id("billing_city");
+        By billingStateDropDown = By.id("billing_state");
+        By billingZipFld = By.id("billing_postcode");
+        By billingEmailFld = By.id("billing_email");
+
+        driver.findElement(billingFirstNameFld).clear();
+        driver.findElement(billingFirstNameFld).sendKeys(billingDetails.get(0).get("firstname"));
+        driver.findElement(billingLastNameFld).clear();
+        driver.findElement(billingLastNameFld).sendKeys(billingDetails.get(0).get("lastname"));
+        driver.findElement(billingAddressOneFld).clear();
+        driver.findElement(billingAddressOneFld).sendKeys(billingDetails.get(0).get("address_line1"));
+        driver.findElement(billingCityFld).clear();
+        driver.findElement(billingCityFld).sendKeys(billingDetails.get(0).get("city"));
+        Select select = new Select(driver.findElement(billingStateDropDown));
+        select.selectByVisibleText(billingDetails.get(0).get("state"));
+        driver.findElement(billingZipFld).clear();
+        driver.findElement(billingZipFld).sendKeys(billingDetails.get(0).get("zip"));
+        driver.findElement(billingEmailFld).clear();
+        driver.findElement(billingEmailFld).sendKeys(billingDetails.get(0).get("email"));
     }
 
     @And("I place an order")
-    public void iPlaceAnOrder() {
+    public void iPlaceAnOrder() throws InterruptedException {
+        By placeOrderBtn = By.id("place_order");
+        driver.findElement(placeOrderBtn).click();
+        Thread.sleep(5000);
+
     }
 
     @Then("the order should be placed successfully")
-    public void theOrderShouldBePlacedSuccessfully() {
+    public void theOrderShouldBePlacedSuccessfully() throws InterruptedException {
+        By noticeTxt = By.cssSelector(".woocommerce-notice");
+        String actualNoticeMsg = driver.findElement(noticeTxt).getText();
+        Assert.assertEquals("Thank you. Your order has been received.", actualNoticeMsg);
+
     }
 }
---
+++ src/test/java/awsomecucumber/hooks/MyHooks.java
@@ -0,0 +1,22 @@
+package awsomecucumber.hooks;
+
+import awsomecucumber.factory.DriverFactory;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import org.openqa.selenium.WebDriver;
+
+public class MyHooks {
+
+    private WebDriver driver;
+
+    @Before
+    public void before(){
+        driver = DriverFactory.initializeDriver();
+
+    }
+
+    @After
+    public void after(){
+        driver.quit();
+    }
+}
